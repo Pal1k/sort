96394737fbfdf9d20d02dc75346031db3376e51e
unknown <p.volintcev@mcl.mk.ua>
2021-11-18 18:05:15 +0200

    Implement selection sort algorytm

---
+++ Source.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include <ctime>
+
+using namespace std;
+
+void printArr(int* arr, int size);
+int* initArr(int size);
+void bubbleSort(int* arr, int size);
+void selectionSort(int* arr, int size);
+
+int main() {
+	int size = 10;
+	int* arr = initArr(size);
+	printArr(arr, size);
+	//bubbleSort(arr, size);
+	selectionSort(arr, size);
+    printArr(arr, size);
+
+
+
+}                   
+
+void printArr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int* initArr(int size)
+{
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 50 + 1;
+	}
+	return arr;
+}
+
+void bubbleSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size -1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+
+			}
+
+		}
+	}
+}
+
+void selectionSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		int tmp = arr[i];
+		int index = i;
+		for (int j = i; j < size; j++) {
+			if (tmp > arr[j]) {
+				tmp = arr[j];
+				index = j;
+			}
+		}
+		swap(arr[i], arr[index]);
+		printArr(arr, size);
+
+			
+	}
+}
