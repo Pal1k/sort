93e7cd7139dc364190e088b247d0aab3eef09830
unknown <p.volintcev@mcl.mk.ua>
2021-11-23 17:51:26 +0200

    Implement selection and insert sorting algorythm

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <ctime>
+#include <chrono> //для функций 
 
 using namespace std;
 
@@ -7,14 +8,19 @@ void printArr(int* arr, int size);
 int* initArr(int size);
 void bubbleSort(int* arr, int size);
 void selectionSort(int* arr, int size);
+void isertSort(int* arr, int size);
 
 int main() {
-	int size = 10;
+	int size = 10000;
 	int* arr = initArr(size);
-	printArr(arr, size);
+	//printArr(arr, size);
+	auto t1 = std::chrono::high_resolution_clock::now();
 	//bubbleSort(arr, size);
 	selectionSort(arr, size);
-    printArr(arr, size);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+	std::cout << duration;
+   // printArr(arr, size);
 
 
 
@@ -62,8 +68,21 @@ void selectionSort(int* arr, int size)
 			}
 		}
 		swap(arr[i], arr[index]);
-		printArr(arr, size);
+		//printArr(arr, size);
 
 			
+	}
+}
+
+void isertSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		int j = i + 1;
+		int tmp = arr[j];
+		for (; j >= 0 && arr[j - 1]; j--) {
+			arr[j] = arr[j - 1];
+		}
+	arr[j] = tmp;
+	//printArr(arr, size);
 	}
 }
